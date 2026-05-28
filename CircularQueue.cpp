2c2aad29818cd73c02508324b6dda9af6f14d286
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-28 11:02:52 +0700

    case 1

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -135,4 +135,11 @@ int main() {
             cin >> ch;
             cout << endl;
 
+            switch (ch)
+            {
+            case '1':
+                {
+                    q.insert();
+                    break;
+                }
             
