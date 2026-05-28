c61fa8d5efba54604e3cd943ac030fe7e5f43179
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-28 11:03:37 +0700

    case 3

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -147,4 +147,9 @@ int main() {
                     q.remove();
                     break;
                 }
+            case '3':
+                {
+                    q.display();
+                    break;
+                }
             
